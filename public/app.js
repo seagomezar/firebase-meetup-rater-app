b60f7b365816585b2ea311fe59e8b5cd8c9ac463
Sebastian Gomez <seagomezar@gmaill.com>
2017-11-23 09:36:13 -0500

    Updating the credentials

--- public/app.js
+++ public/app.js
@@ -1,10 +1,11 @@
 (function () {
     var config = {
-        apiKey: "AIzaSyA-bw8RmdWH08iQ5UmcdtAwT_0hwrrCUBU",
-    authDomain: "superapp-88577.firebaseapp.com",
-    databaseURL: "https://superapp-88577.firebaseio.com",
-    storageBucket: "superapp-88577.appspot.com",
-    messagingSenderId: "294220277117"
+        apiKey: "AIzaSyASPVYRzGyw_1CrVKeSWmDjU6W_RyZ0bag",
+        authDomain: "meetup-rate.firebaseapp.com",
+        databaseURL: "https://meetup-rate.firebaseio.com",
+        projectId: "meetup-rate",
+        storageBucket: "",
+        messagingSenderId: "677180779416"
     };
     firebase.initializeApp(config);
 
@@ -26,58 +27,56 @@
     var consolidado = 0;
     var counter = 0;
 
-    myDatabase.on('value', function(snap) {
+    myDatabase.on('value', function (snap) {
         console.log('Estoy obteniendo todo!!!', snap.val());
         var todosMisRates = snap.val();
     });
 
-    myDatabase.on('child_added', function(snap) {
+    myDatabase.on('child_added', function (snap) {
         console.log('Alguien añadió un dato', snap.val());
         var rate = snap.val();
         alerta.innerHTML = '<strong>' + rate.quien + '</strong> ha calificado este meetup con: <strong>' + rate.cuanto + '</strong> puntos';
-        if(rate.cuanto <= 5) {
+        if (rate.cuanto <= 5) {
             alerta.setAttribute('class', 'alert alert-danger');
-        } else if(rate.cuanto > 5 && rate.cuanto <= 7) {
+        } else if (rate.cuanto > 5 && rate.cuanto <= 7) {
             alerta.setAttribute('class', 'alert alert-warning');
-        } else if(rate.cuanto > 7) {
+        } else if (rate.cuanto > 7) {
             alerta.setAttribute('class', 'alert alert-success');
         }
         alerta.hidden = false;
 
-        setTimeout(function() {
+        setTimeout(function () {
             alerta.hidden = true;
         }, 5000);
 
-        counter ++;
+        counter++;
 
         consolidado = consolidado + parseInt(rate.cuanto, 10);
 
         var total = document.getElementById('total');
 
-        total.innerHTML = 'Éste meetup es un meetup de: ' + parseFloat(consolidado/counter).toFixed(2) + ' puntos. '+counter+' personas han votado.';
+        total.innerHTML = 'Éste meetup es un meetup de: ' + parseFloat(consolidado / counter).toFixed(2) + ' puntos. ' + counter + ' personas han votado.';
 
 
     });
-    myDatabase.on('child_removed', function(snap) {
+    myDatabase.on('child_removed', function (snap) {
         console.log('Alguien removió un dato', snap.val());
     });
-    myDatabase.on('child_changed', function(snap) {
+    myDatabase.on('child_changed', function (snap) {
         console.log('Alguien cambió un dato', snap.val());
     });
 
-    enviar.addEventListener('click', function(){
-        
+    enviar.addEventListener('click', function () {
+
         console.log('quien', quien.value);
         console.log('cuanto', cuanto.value);
 
         // Creemos un objeto con esos datos
 
-        var newRate = {quien: quien.value, cuanto: cuanto.value};
+        var newRate = { quien: quien.value, cuanto: cuanto.value };
 
         myDatabase.push(newRate);
 
     });
-
-
 
 })();
